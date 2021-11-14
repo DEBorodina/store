29cce893bde002453c00277dcc8720afdea56d17
Daria Borodina <dasha.borodina2003@mail.ru>
2021-11-14 16:41:34 +0300

    first add

---
+++ app/Controllers/CatalogController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class CatalogController
+{
+    public function index()
+    {
+        include __DIR__.'/../../views/catalog.php';
+    }
+    public function showProduct()
+    {
+        include __DIR__.'/../../views/product.php';
+    }
+}
